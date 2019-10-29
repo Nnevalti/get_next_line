b2130e72e1c9ad91988be87c17b5ee73757a383c
Valentin DESCHAMPS <vdescham@e2r7p20.42.fr>
2019-10-29 16:17:56 +0100

    modified .h Bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alilin <alilin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: vdescham <vdescham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/14 13:50:34 by alilin            #+#    #+#             */
-/*   Updated: 2019/10/17 15:08:12 by vdescham         ###   ########.fr       */
+/*   Created: 2019/10/29 16:14:31 by vdescham          #+#    #+#             */
+/*   Updated: 2019/10/29 16:14:46 by vdescham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <unistd.h>
 # include <stdlib.h>
